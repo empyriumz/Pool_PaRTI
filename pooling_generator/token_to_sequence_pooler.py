7917147dab817402ba755a564629d3cd35f02e06
Alp Tartici <tartici@sh02-07n07.int>
2025-06-11 11:33:22 -0700

    expanded the usability for already pooled attention matrices

--- pooling_generator/token_to_sequence_pooler.py
+++ pooling_generator/token_to_sequence_pooler.py
@@ -6,13 +6,15 @@ import os
 
 
 class TokenToSequencePooler:
-    def __init__(self, path_token_emb, path_attention_layers):
+    def __init__(self, path_token_emb, path_attention_layers, alpha=0.85):
         # Initialize the pooler by loading token embeddings and attention layers from the given paths.
         # Handles the removal of CLS and END token embeddings from representations.
 
         self.path_token_emb = path_token_emb
         self.path_attention_layers = path_attention_layers
-        self.representations_with_cls = self._load_torch_data(self.path_token_emb).numpy()
+        self.representations_with_cls = self._load_torch_data(self.path_token_emb)
+        self.alpha = alpha
+        
         
         # remove the CLS and END token embedding
         if len(self.representations_with_cls.shape) == 2:
@@ -27,7 +29,7 @@ class TokenToSequencePooler:
         # If the file does not exist, print an error message and return None.
 
         try:
-            tensors = torch.load(file_path)
+            tensors = torch.load(file_path).detach().numpy()
             return tensors
         except:
             print(f"There is no file named {file_path}")
@@ -52,7 +54,7 @@ class TokenToSequencePooler:
         # Perform max pooling across layers by selecting the maximum values across attention layers.
         # Returns the matrix after pooling the attention layers.
     
-        mtx_max_of_max = torch.max(mtx_all_layers[1], dim=1)[0]
+        mtx_max_of_max = torch.max(mtx_all_layers[1], dim=1)[0].numpy()
         return mtx_max_of_max
             
             
@@ -77,15 +79,21 @@ class TokenToSequencePooler:
     
     def pool_parti(self, 
                    verbose=False, 
-                   return_importance=False):
+                   return_importance=False,
+                   alpha = None):
         # Perform pooling based on PageRank algorithm applied to attention matrices.
         # Optionally return importance weights or print details about the importance calculation.
         # Handles errors during the pooling process by printing detailed information.
-                
-        matrix_to_pool = self.create_pooled_matrices_across_layers(mtx_all_layers=self.attn_all_layers).squeeze().numpy()
+
+        if len(self.attn_all_layers.shape) == 2 and self.attn_all_layers.shape[0] == self.attn_all_layers.shape[1]:
+            print(f'You are using a single layer matrix of shape {self.attn_all_layers.shape}. This attention matrix may already be pooled across the heads.')
+            matrix_to_pool = self.attn_all_layers
+        else:
+            matrix_to_pool = self.create_pooled_matrices_across_layers(mtx_all_layers=self.attn_all_layers).squeeze().numpy()
         
 
-        dict_importance = self._page_rank(matrix_to_pool)
+        dict_importance = self._page_rank(matrix_to_pool, 
+                                         alpha = alpha)
         importance_weights = np.array(list(self._calculate_importance_weights(dict_importance).values()))
         
         if return_importance:
@@ -107,10 +115,15 @@ class TokenToSequencePooler:
                 
                 
 
-    def _page_rank(self, attention_matrix, personalization=None, nstart=None, prune_type="top_k_outdegree"):
+    def _page_rank(self, attention_matrix, personalization=None, nstart=None, 
+                   prune_type="top_k_outdegree", 
+                   alpha=None):
         # Run PageRank on the attention matrix converted to a graph.
         # Raises exceptions if the graph doesn't match the token sequence or has no edges.
         # Returns the PageRank scores for each token node.
+
+        if alpha is None:
+            alpha = self.alpha
         
         G = self._convert_to_graph(attention_matrix)
         
@@ -119,7 +132,7 @@ class TokenToSequencePooler:
         if G.number_of_edges() == 0:
             raise Exception(f"You don't seem to have any attention edges left in the graph.") 
         
-        return nx.pagerank(G, alpha=0.85, tol=1e-06, weight='weight', personalization=personalization, nstart=nstart, max_iter=100)
+        return nx.pagerank(G, alpha=alpha, tol=1e-06, weight='weight', personalization=personalization, nstart=nstart, max_iter=100)
         
 
     def _convert_to_graph(self, matrix):
